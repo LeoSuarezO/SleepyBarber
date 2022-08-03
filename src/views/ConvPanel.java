098fe4dc00c4f9c1ad9ab926cff6025f6a3988ac
SebSCam <sebas9903@outlook.com>
2022-08-02 22:20:50 -0500

    background

--- src/views/ConvPanel.java
+++ src/views/ConvPanel.java
@@ -1,7 +1,22 @@
 package views;
 
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JPanel;
+import java.awt.Graphics;
 
 public class ConvPanel extends JPanel {
+    public ConvPanel() {
+        this.setBorder(BorderFactory.createRaisedSoftBevelBorder());
+    }
 
+    @Override
+    public void paint(Graphics g) {
+        g.drawImage(new ImageIcon(getClass().getResource("/images/background.png")).getImage(), 0, 0, getWidth(),
+                getHeight(),
+                this);
+
+        setOpaque(false);
+        super.paint(g);
+    }
 }
